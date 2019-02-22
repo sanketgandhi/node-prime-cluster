3a08de6972df2b863da629d296e38dd8669a65b1
sanketgandhi <sanketgandhi876@gmail.com>
2019-02-22 17:36:06 +0530

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const isPrime = require('./is-prime');
+const app = express();
+
+app.get('/', (req, res) => {
+    const primes = [];
+    const max = Number(req.query.max) || 1000;
+
+    for (let i = 1; i <= max; i++) {
+        if (isPrime(i)) primes.push(i);
+    }
+    res.json(primes);
+});
+
+app.listen(process.env.PORT || 3030);
+
+console.log('app is running!');
+
+module.exports = app;
